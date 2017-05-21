e4bd0894ee82c5e59bc426733c0bf48b88cadb54
ThaoTrinh <trinhthithuthao>
2017-05-21 23:38:12 +0700

    cap nhat file

--- BTL2/Docdulieu.cpp
+++ BTL2/Docdulieu.cpp
@@ -18,6 +18,7 @@ bool Nhap_dulieu(vector<Nguoidung>& Dangnhap){
 		stringstream tach(bien);
 		if (bien.empty())
 			continue;
+		
 		//cout << bien << endl;
 		tach >> bien1 >> bien2 >> bien3;
 		A.Maso = bien1;
@@ -72,60 +73,3 @@ bool Dulieu_chucnang(vector<Nguoidung>& Dangnhap, Nguoidung & Ngdung_dangnhap){
 	return true;
 }
 
-bool Doimatkhau(vector<Nguoidung>& Dangnhap, Nguoidung & Ngdung_dangnhap){
-	system("cls");
-	//Nguoidung C;
-	fstream FILE("dangnhap.txt");
-	if (!FILE.is_open()){
-		cout << "Loi mo file";
-		return false;
-	}
-	cin.ignore();
-	cout<<"Ban muon doi mat khau? (y/n)";
-	string y;
-	getline(cin,y);
-	if(y!="y") return false;
-	else{//that do, khong nen dung kiemtra,
-	//hoi nay chay dang nhap kiemtra = true
-	//gio !kiemtra = false
-		bool b=false;
-		while(!b){
-			cout<<"Nhap mat khau cu: ";
-			string matkhau;
-			getline(cin,matkhau);
-			if(matkhau==Ngdung_dangnhap.Matkhau){
-				cout<<"Nhap mat khau moi: ";
-				string matkhaumoi;
-				getline(cin,matkhaumoi);
-
-				while(true){
-					cout<<"Nhap lai mat khau moi: ";
-					//neu ma muon thoat??????
-					string nhaplai;
-					getline(cin,nhaplai);
-					if(nhaplai!=matkhaumoi){
-						continue;
-					}
-					else {
-						Dangnhap[Ngdung_dangnhap.Maso-1].Matkhau=matkhaumoi;
-						//cap nhat lai file 
-
-						return true;
-					}
-				}
-			}//if
-			else{
-				cout<<"Mat khau cu khong chinh xac"<<endl;
-				cout<<"Ban co muon nhap lai khong?(y/n)";
-				string y;
-				getline(cin,y);
-				if(y!="y") return false;
-				else{
-					b=false;
-				}
-			}//else
-		}//ket thuc while
-	}
-	FILE.close();
-	return true;
-}
