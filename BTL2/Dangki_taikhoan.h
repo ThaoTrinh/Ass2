c630f2527c21791ac3e749f28a1e4bbcb9b08bc1
ThaoTrinh <trinhthithuthao>
2017-05-21 23:36:12 +0700

    cap nhat file

---
+++ BTL2/Dangki_taikhoan.h
@@ -0,0 +1,13 @@
+#ifndef DANGKI
+#define DANGKI
+
+#include "thuvien_chuan.h"
+bool Dangki(std::vector<Nguoidung>& Dangnhap);
+
+bool thongtin_dangki(
+  std::vector<thongtin_nguoidung>& Dangki_taikhoan,
+  Nguoidung& Ngdung_dangnhap,
+  std::vector<Nguoidung>& Dangnhap) ;
+
+bool Chucnang_dangki(std::vector<thongtin_nguoidung>& Dangki_taikhoan, Nguoidung& Ngdung_dangnhap,std::vector<Nguoidung>& Dangnhap);
+#endif
